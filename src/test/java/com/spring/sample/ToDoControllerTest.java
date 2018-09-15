8b21758fbb9866dae4f517e719e4a3b73e6c4263
Koichi Taura <taura.koichi@gmail.com>
2018-09-15 11:26:31 +0900

    [add] index and detail abnormal test

--- src/test/java/com/spring/sample/ToDoControllerTest.java
+++ src/test/java/com/spring/sample/ToDoControllerTest.java
@@ -1,5 +1,7 @@
 package com.spring.sample;
 
+import com.spring.sample.Exception.NotFoundException;
+import com.spring.sample.controller.ExceptionHandleController;
 import com.spring.sample.controller.ToDoController;
 import com.spring.sample.entity.ToDoEntity;
 import com.spring.sample.service.ToDoService;
@@ -8,22 +10,29 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.exceptions.misusing.MockitoConfigurationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
 public class ToDoControllerTest {
 
     @Mock
     private ToDoService toDoService;
+
+    @Spy
+    private ExceptionHandleController exceptionHandleController;
 
     @InjectMocks
     private ToDoController toDoController;
@@ -50,10 +59,13 @@ public class ToDoControllerTest {
         ModelAndView mav = new ModelAndView();
         ModelAndView actual = toDoController.index(mav);
         ModelMap modelMap = actual.getModelMap();
-        List<ToDoEntity> actualToDoEntityList = (List<ToDoEntity>) modelMap.get("todo_list");
 
         assertEquals("index.html", actual.getViewName());
         assertTrue(modelMap.containsKey("todo_list"));
+
+        List<ToDoEntity> actualToDoEntityList = (List<ToDoEntity>) modelMap.get("todo_list");
+
+        assertEquals(10, actualToDoEntityList.size());
         assertEquals(new Integer(1), actualToDoEntityList.get(0).getId());
         assertEquals("第1回目Springの勉強する", actualToDoEntityList.get(0).getTodo());
         assertEquals("Taurin189", actualToDoEntityList.get(0).getPersonInCharge());
@@ -63,5 +75,58 @@ public class ToDoControllerTest {
         assertEquals(new Integer(10), actualToDoEntityList.get(9).getId());
         assertEquals("第10回目Springの勉強する", actualToDoEntityList.get(9).getTodo());
         assertEquals("Taurin189", actualToDoEntityList.get(9).getPersonInCharge());
+    }
+
+    @Test
+    public void indexWithNoToDoList() {
+        List<ToDoEntity> entityList = new ArrayList<ToDoEntity>();
+        when(toDoService.findAll()).thenReturn(entityList);
+        ModelAndView mav = new ModelAndView();
+        ModelAndView actual = toDoController.index(mav);
+        ModelMap modelMap = actual.getModelMap();
+
+        assertEquals("index.html", actual.getViewName());
+        assertTrue(modelMap.containsKey("todo_list"));
+
+        List<ToDoEntity> actualToDoEntityList = (List<ToDoEntity>) modelMap.get("todo_list");
+
+        assertEquals(0, actualToDoEntityList.size());
+        }
+
+    @Test
+    public void detail() {
+        ToDoEntity entity = ToDoEntity.builder()
+                .id(1)
+                .todo("第1回目Springの勉強する")
+                .personInCharge("Taurin189")
+                .isFinished(false)
+                .build();
+        when(toDoService.findById(1)).thenReturn(entity);
+
+        ModelAndView mav = new ModelAndView();
+        ModelAndView actual = toDoController.detail(1, mav);
+        ModelMap modelMap = actual.getModelMap();
+
+        assertEquals("detail.html", actual.getViewName());
+        assertTrue(modelMap.containsKey("todo"));
+
+        ToDoEntity actualEntity = (ToDoEntity) modelMap.get("todo");
+
+        assertEquals(new Integer(1), actualEntity.getId());
+        assertEquals("第1回目Springの勉強する", actualEntity.getTodo());
+        assertEquals("Taurin189", actualEntity.getPersonInCharge());
+        assertFalse( actualEntity.isFinished());
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void detailNotFound() {
+        when(toDoService.findById(10)).thenThrow(new NotFoundException("ID: " + String.valueOf(10) + " Not Found"));
+
+        ModelAndView mav = new ModelAndView();
+        ModelAndView actual = toDoController.detail(10, mav);
+        ModelMap modelMap = actual.getModelMap();
+
+        assertEquals("error.html", actual.getViewName());
+        assertEquals(HttpStatus.NOT_FOUND, actual.getStatus());
     }
 }
